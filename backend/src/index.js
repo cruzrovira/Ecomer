3c0e5bf0d3c9567feec90b539daba6def809cc7d
Oscar Rovira <cruzrovira@gmail.com>
2021-08-26 04:50:52 -0600

    adicion de frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use("/", productRoute);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log("servidor cargado en ");
---
+++ frontend/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>hola mundo </h1>;
+}
+
+export default App;
